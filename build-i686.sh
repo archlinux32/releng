ac9c5c52532683dae78146ce5194b4243c2698f3
Tyler Dence <tyzoid.d@gmail.com>
2018-05-02 06:43:45 -0400

    Switch the iso build scripts to use releng instead of archlinuxewe

--- build-i686.sh
+++ build-i686.sh
@@ -13,54 +13,26 @@ cat << "__ENDOFPROVISION.SH__" > provision.sh
 #!/bin/bash
 set -e
 ln -svf /usr/share/zoneinfo/America/Detroit /etc/localtime
-tmpFile="$(mktemp)"
-curl -o "${tmpFile}" "https://arch.eckner.net/archlinuxewe/masterkeys.gpg"
-pacman-key --add "${tmpFile}"
-rm -f "${tmpFile}"
-pacman-key --lsign-key 5FDCA472AB93292BC678FD59255A76DB9A12601A
-pacman-key --lsign-key F8028D351891AE15970A2B3B3CFB0AD8F60030F8
-if ! grep -q "^Server = https://arch\.eckner\.net" /etc/pacman.d/mirrorlist
-then
-  ml="$(
-    curl "https://arch.eckner.net/archlinuxewe/os/any/" 2> /dev/null | \
-      tr "<>" "\n\n" | \
-      grep "^pacman-mirrorlist-.*\.pkg\.tar\.xz\$" | \
-      tail -n1
-  )"
-  curl "https://arch.eckner.net/archlinuxewe/os/any/${ml}" 2> /dev/null | \
-    tar -OxJ etc/pacman.d/mirrorlist > \
-    /etc/pacman.d/mirrorlist
-fi
-if ! grep -q "^\[archlinuxewe\]\$" /etc/pacman.conf
-then
-  tmpFile="$(mktemp)"
-  cat /etc/pacman.conf | \
-    (
-      while read s
-      do
-        if [[ "$s" = "# The testing repositories"* ]]
-        then
-          echo '[archlinuxewe]'
-          echo 'SigLevel = Required'
-          echo 'Include = /etc/pacman.d/mirrorlist'
-          echo ''
-        fi
-        echo "${s}"
-      done
-    ) > "${tmpFile}"
-  cat "${tmpFile}" > /etc/pacman.conf
-  rm -f "${tmpFile}"
-fi
 
-sudo pacman --noconfirm -Sy archlinux-keyring archlinux32-keyring
-sudo pacman --noconfirm -Syu archiso32
+pacman --noconfirm -Sy archlinux-keyring
+
+tee -a /etc/pacman.conf << "_PACMANCONF_"
+[releng]
+Include = /etc/pacman.d/mirrorlist32
+_PACMANCONF_
+
 cat << "__ENDOFARCH32MIRRORLIST__" > /etc/pacman.d/mirrorlist32
 Server = https://32.arlm.tyzoid.com/$arch/$repo
 Server = http://arch32.mirrors.simplysam.us/$arch/$repo
 Server = https://mirror.archlinux32.org/$arch/$repo
 __ENDOFARCH32MIRRORLIST__
 
-cat << "__ENDOFISOBUILDSCRIPT__" | sudo tee /root/buildiso.sh >/dev/null
+pacman --noconfirm -Sy archlinux32-keyring-transition
+pacman --noconfirm -R archlinux32-keyring-transition
+pacman --noconfirm -S archlinux32-keyring
+pacman --noconfirm -Syu archiso32
+
+cat << "__ENDOFISOBUILDSCRIPT__" | tee /root/buildiso.sh >/dev/null
 #!/bin/bash
 /usr/share/archiso/configs/releng/build.sh -v -V"$(date -d"$(date -d "+2day" +%Y-%m-01T12:00:00Z)" +%Y.%m.%d)" -L"ARCH_$(date -d"$(date -d "+2day" +%Y-%m-01T12:00:00Z)" +%Y%m)"
 __ENDOFISOBUILDSCRIPT__
